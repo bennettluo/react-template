ceb9b8505fac83533e05497f7c51ad9bdf2314bc
Yuetong Yang <recoyang1992@gmail.com>
2020-01-16 14:30:49 +0800

    set up prettier

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,13 @@
-import React from "react";
-import Button from "@material-ui/core/Button";
-import "./App.css";
+import React from 'react';
+import Button from '@material-ui/core/Button';
+import './App.css';
 
-const App: React.FC = () => {
-  return (
-    <Button variant="contained" color="primary">
-      Hello World
-    </Button>
-  );
+const App = () => {
+    return (
+        <Button variant="contained" color="primary">
+            Hello World
+        </Button>
+    );
 };
 
 export default App;
